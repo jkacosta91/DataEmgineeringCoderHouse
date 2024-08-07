46d7830257a85b9eec41169e8cd675d48c761f7b
Jonathan Acosta <jkacosta91@gmail.com>
2024-08-07 19:18:43 +0200

    realizando carga de datos

---
+++ Primera Entrega/modules/__init__.py
@@ -0,0 +1,2 @@
+from .data_con import DataConn
+from .get_data_from_api import DataRetriever
---
+++ Primera Entrega/sql_scrips/sql_query_creation.sql
@@ -0,0 +1,11 @@
+DROP TABLE IF EXISTS jkacosta91_coderhouse.Data_Coder;
+
+CREATE TABLE Data_Coder(
+
+    id VARCHAR(250),
+    name VARCHAR(250),
+    full_name VARCHAR(250),
+    private VARCHAR(250),
+    html_url VARCHAR(250),
+    size VARCHAR(250)
+);
